f643a0fc112a08b96ed3085427db707cd02c00b6
gaumeister <derek@apigrate.com>
2023-08-22 09:36:40 -0500

    bugfix OrganizationalResources spelling

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ class AutotaskRestApi {
       {name:'OrganizationalLevel1s'},
       {name:'OrganizationalLevel2s'},
       {name:'OrganizationalLevelAssociations'},
-      {name:'OrganizatonalResources', childOf: 'OrganizationalLevelAssociations', subname: 'Resources'},
+      {name:'OrganizationalResources', childOf: 'OrganizationalLevelAssociations', subname: 'Resources'},
       {name:'PaymentTerms'},
       {name:'Phases', childOf: 'Projects', subname: 'Phases'},
       {name:'PriceListMaterialCodes'},
